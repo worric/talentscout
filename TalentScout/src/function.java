ac54a751e8501443ab958ce3dff91385f2a5f775
mikkelmrch <moerch.mikkel@gmail.com>
2015-11-23 15:12:01 +0100

    Kommentarerne der siger //CURRENT FUNCTION indikerer søgefunktionen som der arbejdes på. Der er et par dubletter.

--- TalentScout/src/function.java
+++ TalentScout/src/function.java
@@ -21,89 +21,7 @@ public class function {
         
     }
     
-    
-    
-    public void encapsulateAttributes(){
-        
-    }
-    
-    /**
-     * Finds Player objects whose names match with the given user input and adds them
-     * to an ArrayList. 
-     * @param inputName The name searched upon by the user
-     * @param inputClub Club searched upon by the user
-     * @param inputAge Age searched on
-     * @return An ArrayList of Player objects
-     * @throws Exception 
-     */
-    public ArrayList<Player> search(String inputName, String inputClub, String inputAge)throws Exception{
-        // ArrayList of Player objects
-        ArrayList<Player> foundPlayers = new ArrayList();
-        Player plrRestore;
-        boolean checkInputName;
-        boolean checkInputClub;
-        boolean checkInputAge;
-        // Converts the first letter of the input String into a capital letter
-        // String inputUP = input.toUpperCase();
-        // String inputReady = inputUP.substring(1).toLowerCase();
-       
-        // Declaring the directory of player objects 
-        File[] files = getFiles();
-        
-        // Loops through the directory of player objects 
-        for(int i = 0; i < files.length; i++){
-
-            // Open each player object file
-            plrRestore = open(files[i].getName());
-            // Convert the int age to a String in order to be able to compare it
-            // with user input via the matches() method. 
-            String age = Integer.toString(plrRestore.age);
-            
-            // User input we want to check if it matches with any Player object's attributes
-            // Given variables to give a better overview.
-                        
-            // If the field is empty let the boolean be true. This is so that the
-            // user do not need to fill in text in all search fields.
-            if(inputName.isEmpty()){
-                checkInputName = true;
-            } else {
-                checkInputName = plrRestore.name.matches(inputName + ".*");
-            }
-            
-            if(inputClub.isEmpty()){
-                checkInputClub = true;
-            } else {
-                checkInputClub = plrRestore.Club.matches(inputClub + ".*");
-            }
-            
-            if(inputAge.isEmpty()){
-                checkInputAge = true;
-            } else {
-                checkInputAge = age.matches(inputAge + ".*");
-            }
-            
-            // If all the inputfields are empty return all players to the table.
-                // .isEmpty() returns true if String.length() == 0.
-            // That is, if the user did not type anything into the textfield.
-            if(inputName.isEmpty() && inputClub.isEmpty() && inputAge.isEmpty()){
-                foundPlayers.add(plrRestore);
-            
-            // Checks if the user inputs match the Player object's attributes.
-            // Also due to code above, this returns true if some search fields are left blank
-            // and one search field has a value which matches the attribute of a Player object.
-            } else if(checkInputName && checkInputClub && checkInputAge){
-            
-                //if true, add the player object to the ArrayList
-                foundPlayers.add(plrRestore);
-            } else {
-                // if false, that is if there are no matches we display an empty table
-                foundPlayers.clear();
-            }
-        }
-        // return the ArrayList with all the players found. 
-        return foundPlayers;
-    }
-    
+    // CURRENT FUNCTION BEEN WORKED ON
     public ArrayList<Player> searchForName(String inputName)throws Exception{
         // ArrayList of Player objects
         ArrayList<Player> foundPlayers = new ArrayList();
@@ -134,6 +52,7 @@ public class function {
         return foundPlayers;
     }
     
+    // CURRENT FUNCTION BEEN WORKED ON
      public void InsertSearchResultToTableName(String inputName, DefaultTableModel model) throws Exception{
 
         // ArrayList of players found from search
@@ -147,15 +66,123 @@ public class function {
                 String age = Integer.toString(plrRestore.age);
                 
                 // Make a String array of the players' attributes
-                String[] data = {plrRestore.name, age, plrRestore.Club};
+                String[] data = {plrRestore.name, age, plrRestore.club};
                 
                 // Add the array to a row in the table
                 model.addRow(data);
             }
     }
     
+     
+     // DUBPLET 
+     /**
+      * Finds Player objects whose names match with the given user input and adds them
+      * to an ArrayList. 
+      * @param inputName The name searched upon by the user
+      * @param inputClub club searched upon by the user
+      * @param inputAge Age searched on
+      * @return An ArrayList of Player objects
+      * @throws Exception 
+      */
+     public ArrayList<Player> search(String inputName, String inputClub, String inputAge)throws Exception{
+         // ArrayList of Player objects
+         ArrayList<Player> foundPlayers = new ArrayList();
+         Player plrRestore;
+         boolean checkInputName;
+         boolean checkInputClub;
+         boolean checkInputAge;
+         // Converts the first letter of the input String into a capital letter
+         // String inputUP = input.toUpperCase();
+         // String inputReady = inputUP.substring(1).toLowerCase();
+        
+         // Declaring the directory of player objects 
+         File[] files = getFiles();
+         
+         // Loops through the directory of player objects 
+         for(int i = 0; i < files.length; i++){
+
+             // Open each player object file
+             plrRestore = open(files[i].getName());
+             // Convert the int age to a String in order to be able to compare it
+             // with user input via the matches() method. 
+             String age = Integer.toString(plrRestore.age);
+             
+             // User input we want to check if it matches with any Player object's attributes
+             // Given variables to give a better overview.
+                         
+             // If the field is empty let the boolean be true. This is so that the
+             // user do not need to fill in text in all search fields.
+             if(inputName.isEmpty()){
+                 checkInputName = true;
+             } else {
+                 checkInputName = plrRestore.name.matches(inputName + ".*");
+             }
+             
+             if(inputClub.isEmpty()){
+                 checkInputClub = true;
+             } else {
+                 checkInputClub = plrRestore.club.matches(inputClub + ".*");
+             }
+             
+             if(inputAge.isEmpty()){
+                 checkInputAge = true;
+             } else {
+                 checkInputAge = age.matches(inputAge + ".*");
+             }
+             
+             // If all the inputfields are empty return all players to the table.
+                 // .isEmpty() returns true if String.length() == 0.
+             // That is, if the user did not type anything into the textfield.
+             if(inputName.isEmpty() && inputClub.isEmpty() && inputAge.isEmpty()){
+                 foundPlayers.add(plrRestore);
+             
+             // Checks if the user inputs match the Player object's attributes.
+             // Also due to code above, this returns true if some search fields are left blank
+             // and one search field has a value which matches the attribute of a Player object.
+             } else if(checkInputName && checkInputClub && checkInputAge){
+             
+                 //if true, add the player object to the ArrayList
+                 foundPlayers.add(plrRestore);
+             } else {
+                 // if false, that is if there are no matches we display an empty table
+                 foundPlayers.clear();
+             }
+         }
+         // return the ArrayList with all the players found. 
+         return foundPlayers;
+     }
+     
+     // DUBLET
+     /**
+      * Inserts Player objects from an ArrayList into a table.
+      * @param inputName name searched on
+      * @param inputClub club searched on
+      * @param inputAge age searched on
+      * @param model Table we want to insert Players into 
+      * @throws Exception 
+      */
+     public void InsertSearchResultToTable(String inputName, String inputClub, String inputAge, DefaultTableModel model) throws Exception{
+
+         // ArrayList of players found from search
+         ArrayList<Player> playersFound = search(inputName, inputClub, inputAge);
+         
+             // Loop through the players in the ArrayList
+             for(int i = 0; i < playersFound.size(); i++){
+                 
+                 Player plrRestore = playersFound.get(i);
+                 // Convert player age from type int to String
+                 String age = Integer.toString(plrRestore.age);
+                 
+                 // Make a String array of the players' attributes
+                 String[] data = {plrRestore.name, age, plrRestore.club};
+                 
+                 // Add the array to a row in the table
+                 model.addRow(data);
+             }
+     }
+     
     
-    
+    // DUBLET AF SØGEFUNKTION
     public ArrayList<Player> searchForClub(String inputClub)throws Exception{
         // ArrayList of Player objects
         ArrayList<Player> foundPlayers = new ArrayList();
@@ -180,7 +207,7 @@ public class function {
             // Checks if the user inputs match the Player object's attributes.
             // Also due to code above, this returns true if some search fields are left blank
             // and one search field has a value which matches the attribute of a Player object.
-            } else if(plrRestore.Club.matches(inputClub + ".*")){
+            } else if(plrRestore.club.matches(inputClub + ".*")){
             
                 //if true, add the player object to the ArrayList
                 foundPlayers.add(plrRestore);
@@ -193,18 +220,8 @@ public class function {
         return foundPlayers;
     }
     
-    public void buildString(){
-        
-        String[] tokens = {"lol", "rofl", "mao"};
-        StringBuilder sb = new StringBuilder();
-        sb.append("(?i)^" + "(?=.*" + tokens[0] + ")");
-            for (int num = 0; num < tokens.length; num++) {
-                sb.append("(?=.*" + tokens[num] + ")");
-                
-        }
-        System.out.println(sb);
-    }
     
+    // DUBLET AF SØGEFUNKTION
     public void InsertSearchResultToTableClub(String inputClub, DefaultTableModel model) throws Exception{
 
         // ArrayList of players found from search
@@ -218,41 +235,14 @@ public class function {
                 String age = Integer.toString(plrRestore.age);
                 
                 // Make a String array of the players' attributes
-                String[] data = {plrRestore.name, age, plrRestore.Club};
+                String[] data = {plrRestore.name, age, plrRestore.club};
                 
                 // Add the array to a row in the table
                 model.addRow(data);
             }
     }
    
-    /**
-     * Inserts Player objects from an ArrayList into a table.
-     * @param inputName name searched on
-     * @param inputClub club searched on
-     * @param inputAge age searched on
-     * @param model Table we want to insert Players into 
-     * @throws Exception 
-     */
-    public void InsertSearchResultToTable(String inputName, String inputClub, String inputAge, DefaultTableModel model) throws Exception{
 
-        // ArrayList of players found from search
-        ArrayList<Player> playersFound = search(inputName, inputClub, inputAge);
-        
-            // Loop through the players in the ArrayList
-            for(int i = 0; i < playersFound.size(); i++){
-                
-                Player plrRestore = playersFound.get(i);
-                // Convert player age from type int to String
-                String age = Integer.toString(plrRestore.age);
-                
-                // Make a String array of the players' attributes
-                String[] data = {plrRestore.name, age, plrRestore.Club};
-                
-                // Add the array to a row in the table
-                model.addRow(data);
-            }
-    }
-    
     /**
      * Changes the card in a cardLayout container
      * @param parent the container in which we want to change layout
@@ -282,7 +272,7 @@ public class function {
         Player plr = new Player();
         plr.name = name;
         plr.age = age;
-        plr.Club = club;
+        plr.club = club;
         
         return plr;
     }
@@ -354,42 +344,6 @@ public class function {
     }
     
     /**
-     * Iterates through all the attributes of a Player Object prints its values
-     * to the console.
-     * @param plr Player Object
-     * @throws IllegalAccessException 
-     */
-    public void printAttributeValues(Player plr) throws IllegalAccessException{
-        
-            // Gets the class of the Object
-            Class<?> c = plr.getClass();
-            // getDeclaredField() returns an array of type Field of the Fields of 
-            // a class. That is, public, private, protected, instance and static variables.
-            // Only inherited Fields are not returned.
-            Field[] attributes = c.getDeclaredFields();
-            //Loop through the array of Fields
-            for(int j = 0; j < attributes.length; j++){
-                // get(Object) returns the value of a Field of the specified Object. The value is wrapped in an Object.
-                Object o = attributes[j].get(plr);
-                
-                // converting the Object to a String.
-                String str = o.toString();
-                System.out.println(str);
-            }
-    }
-    
-    public ArrayList<Object> loopAttributes(Player plr) throws IllegalAccessException{
-        ArrayList<Object> attributes = new ArrayList();
-        Field[] fields = plr.getClass().getDeclaredFields();
-        for(int i = 0; i < fields.length; i++){
-               Object o = fields[i].get(plr);
-               attributes.add(o);
-               
-        }
-        return attributes;
-    }
-    
-    /**
      * Checks if the directory for player files is created.
      * If it isn't, we create it using mkdir(). 
      */
@@ -425,4 +379,53 @@ public class function {
         return paths;
 
         }
+    }
+
+	// OUT-OF-CONTEXT FUNCTIONS BEING RESEARCHED
+    /**
+     * Iterates through all the attributes of a Player Object prints its values
+     * to the console.
+     * @param plr Player Object
+     * @throws IllegalAccessException 
+     */
+    public void printAttributeValues(Player plr) throws IllegalAccessException{
+        
+            // Gets the class of the Object
+            Class<?> c = plr.getClass();
+            // getDeclaredField() returns an array of type Field of the Fields of 
+            // a class. That is, public, private, protected, instance and static variables.
+            // Only inherited Fields are not returned.
+            Field[] attributes = c.getDeclaredFields();
+            //Loop through the array of Fields
+            for(int j = 0; j < attributes.length; j++){
+                // get(Object) returns the value of a Field of the specified Object. The value is wrapped in an Object.
+                Object o = attributes[j].get(plr);
+                
+                // converting the Object to a String.
+                String str = o.toString();
+                System.out.println(str);
+            }
+    }
+    
+    public ArrayList<Object> loopAttributes(Player plr) throws IllegalAccessException{
+        ArrayList<Object> attributes = new ArrayList();
+        Field[] fields = plr.getClass().getDeclaredFields();
+        for(int i = 0; i < fields.length; i++){
+               Object o = fields[i].get(plr);
+               attributes.add(o);
+               
+        }
+        return attributes;
+    }
+    
+    public void buildString(){
+        
+        String[] tokens = {"lol", "rofl", "mao"};
+        StringBuilder sb = new StringBuilder();
+        sb.append("(?i)^" + "(?=.*" + tokens[0] + ")");
+            for (int num = 0; num < tokens.length; num++) {
+                sb.append("(?=.*" + tokens[num] + ")");
+                
+        }
+        System.out.println(sb);
     }
