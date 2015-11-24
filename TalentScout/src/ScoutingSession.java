0c27abc1a1ec19b1d3de8e47f460ca3615b15fb7
HappyFisk <frederiknygart@gmail.com>
2015-11-24 15:53:19 +0100

    More doc!!!

--- TalentScout/src/ScoutingSession.java
+++ TalentScout/src/ScoutingSession.java
@@ -36,8 +36,8 @@ public class ScoutingSession {
 	 */
 	public ScoutingSession(String location, Date date, String sessionID){
 		players = new ArrayList <Player>();
-		this.location = location;
-		this.date = date;
+		this.setLocation(location);;
+		this.setDate(date);
 		this.setSessionID(sessionID);
 	}
 	
@@ -53,6 +53,36 @@ public class ScoutingSession {
 	
 	
 	/**
+	 * Sets a ScoutingSession's location
+	 * @param location is of the type String and should be the location of where the ScoutingSession takes place
+	 * @see #getLocation()
+	 */
+	public void setLocation(String location){
+		this.location = location;
+	}
+	
+	
+	/**
+	 * Gets a ScoutingSession's date
+	 * @return this ScoutingSessions date
+	 * @see #date 
+	 */
+	public Date getDate() {
+		return date;
+	}
+
+
+	/**
+	 * Sets a ScoutingSession's date
+	 * @param date is of the type Date and should be the exact time of when the ScoutingSession takes place
+	 * @see #getDate()
+	 */
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	
+	
+	/**
 	 * Gets a ScoutingSession's ID
 	 * @return the ScoutingSession's ID
 	 * @see #setSessionID(String)
@@ -62,9 +92,10 @@ public class ScoutingSession {
 		return sessionID;
 	}
 
+	
 	/**
 	 * Sets a ScoutingSession's ID
-	 * @param sessionID is a unique ID for this ScoutingSession
+	 * @param sessionID is a String which should be an unique ID for this ScoutingSession
 	 * @see #getSessionID
 	 */
 	public void setSessionID(String sessionID) {
