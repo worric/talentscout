a1bc3af3b85c733b669512ce3843c5db8fa9b735
Robin Larsen <rdalarsen@gmail.com>
2015-12-01 10:51:24 +0100

    merged agenda from database branch to the master branch

--- TalentScout/src/Agenda.java
+++ TalentScout/src/Agenda.java
@@ -17,10 +17,8 @@ public class Agenda {
 	
 	/**ArrayList with ScoutingSessions referred to as "sessions".*/
 	private ArrayList <ScoutingSession> sessions;
-	 
-	///** A generic ScoutingSession referred to as "session".*/
-	//private ScoutingSession session;
 	
+	// Delare the variables needed for reading/writing data to/from disk
 	private String path;
 	private String fileName;
 	private String pathForFile;
@@ -28,14 +26,74 @@ public class Agenda {
 	// Class variable for generating ID for the planned session
 	private static int idCounter = 0;
 	
+	// Enables the class to access operations in PlayerDB class
 	private PlayerDB pdb;
 	
 	
 	/**Creates an agenda which ties the reference variable "sessions" to a new ArrayList*/
-	public Agenda(){
-		sessions = new ArrayList<ScoutingSession>();
+	public Agenda(PlayerDB pdb){
+		// Initializing the variables needed for reading/writing data to/from disk
+		path = "./playerfiles";
+		fileName = "agendadb";
+		pathForFile = path+"/"+fileName;
+		
+		// enables the class to access operations in PlayerDB class
+		this.pdb = pdb;
+		
+		// Loads the previously saved databases on the hard drive, if any
+		sessions = this.loadAgenda();
 	}
 	
+	/**
+	 * Save the current list of scouting session to disk
+	 **/
+	public void saveAgenda(){
+    		try {
+    			//Connecting to the file
+    			FileOutputStream fo = new FileOutputStream(pathForFile);
+    		
+    			//Opening the file
+    			ObjectOutputStream oo = new ObjectOutputStream(fo);
+    		
+    			// Writing to the file 
+    			oo.writeObject(this.sessions);
+    			
+    			// Close the stream
+    			oo.close();
+    		} catch (Exception e){
+    			e.printStackTrace();
+    		}
+    	}
+    
+	/**
+	* Load a file which contains all agenda objects
+	* @return
+	*/
+	public ArrayList<ScoutingSession> loadAgenda(){
+		try {
+        		// we check if the folder/directory exists
+        		checkDirectoryExist();
+        
+    			// Connection established to the file
+    			FileInputStream fi = new FileInputStream(pathForFile);
+    	
+    			// Preparing for reading of file
+    			ObjectInputStream oi = new ObjectInputStream(fi);
+    	
+    			// Reading the file 
+    			@SuppressWarnings("unchecked")
+    			ArrayList<ScoutingSession> agendaArray = (ArrayList<ScoutingSession>) oi.readObject();
+    	
+    			//Closing the stream
+    			oi.close();
+    		
+    			return agendaArray;
+    	
+    		} catch (Exception e){
+    			e.printStackTrace();
+    			return new ArrayList<ScoutingSession>();
+    		}
+    	}
 	
 	/**
 	 * Plans a ScoutingSession by creating a new ScoutingSession and thereafter adding it to the ArrayList "sessions". It also initiates a new ScoutingSession with an unique sessionID.
@@ -44,21 +102,11 @@ public class Agenda {
 	 * @return ScoutingSession(location, date)
 	 * */
 	public ScoutingSession planSession(String location, Date date){
-		String sessionID = generateSessionID();
-		ScoutingSession session = new ScoutingSession(location, date, sessionID);
+		ScoutingSession session = new ScoutingSession(location, date, Agenda.idCounter);
 		sessions.add(session);
+		Agenda.idCounter++;
 		return session;
 	}
-	
-	/**
-	 * Creates an unique sessionID which is given to an instance of the class ScoutingSession;
-	 * @return a String with "Session" + sessions.size()
-	 * @see #planSession(String, Date)
-	 */
-	public String generateSessionID(){
-		return "Session" + sessions.size();
-	}
-	
 	
 	/**
 	 * This method cancels an already planned ScoutingSession.
@@ -70,27 +118,22 @@ public class Agenda {
 		sessions.remove(index);
 	}
 	
-	
 	/**
 	 * Gets a ScoutingSession from the ArrayList "sessions".
 	 * @param index is an integer value which decides the place of action in the ArrayList
 	 * @return ScoutingSession
 	 */
-	public ScoutingSession getSession(int index){
-		if(index > sessions.size()){
-			return null;		
-		}else{
-			return sessions.get(index);	
-		}
-
+	public ScoutingSession getSessionByIndex(int index){
+		return sessions.get(index);
 	}
 	
-	
-	public ScoutingSession findSession(String sessionID){
-		for(int i = 0; i < sessions.size(); i++){
-			ScoutingSession session = getSession(i);
-			if(sessionID.equals(session.getSessionID())){
-				return session;
+	/**
+	 * Gets a ScoutingSession from the ArrayList "sessions" based on the ID of the object
+	 */
+	public ScoutingSession getSessionByID(int id){
+		for(ScoutingSession s : sessions){
+			if (s.getSessionID() == id){
+				return s;
 			}
 		}
 		return null;
@@ -112,6 +155,27 @@ public class Agenda {
 		for(int i = 0; i < sessions.size(); i = i + 1){
 			displaySession(i);
 		}
+	}
+	
+	/**
+	 * Returns an ArrayList of scouting sessions sorted by date to be presented in the GUI
+	 */
+	public ArrayList<ScoutingSession> getSortedList(){
+		// Clone the current list of scouting sessions
+		ArrayList<ScoutingSession> list = (ArrayList<ScoutingSession>)sessions.clone();
+		
+		// Perform the actual sorting of the list, based on date
+		Collections.sort(list);
+		
+		// Return the sortes list
+		return list;
+	}
+	
+	/**
+	 * Returns the total number of sessions in the ArrayList - primarily used by other classes when iterating through scouting sessions.
+	 */
+	public int getNumberOfSessions(){
+		return sessions.size();
 	}
 
 	
