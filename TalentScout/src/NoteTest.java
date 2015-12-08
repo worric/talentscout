fef4ad384400e1baeab7a9c42a62cd329ef37764
worric <rdalarsen@gmail.com>
2015-12-08 13:25:01 +0100

    updated the note test class

--- TalentScout/src/NoteTest.java
+++ TalentScout/src/NoteTest.java
@@ -1,87 +1,197 @@
 import static org.junit.Assert.*;
+
+import java.io.File;
+import java.util.ArrayList;
 import java.util.Date;
 import org.junit.BeforeClass;
 import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 public class NoteTest {
 	
-	static int sessionID;
+	static String name;
+	static int age;
+	static String club;
+	
+	static Date date;
+	static String location;
+	
+	static String speedText;
+	static int speedScore;
+	static String attitudeText;
+	static int attitudeScore;
+	static String techniqueText;
+	static int techniqueScore;
+	static String gameSenseText;
+	static int gameSenseScore;
+	
+	/*static int sessionID;
 	static int playerID;
 	static Note n;
 	static Note n2;
 	static ScoutingSession s;
 	static Player p;
-	static Player p2;
+	static Player p2;*/
 
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
 		// sesssion setup
-		Date date = UserInterface.DATEMANAGER.fromStringToDate("27-05-2015");
-		String location = "Skodsborg";
-		s = UserInterface.AGENDA.planSession(location, date);
-		sessionID = s.getSessionID();
+		date = UserInterface.DATEMANAGER.fromStringToDate("27-05-2015");
+		location = "Skodsborg";
 		
 		// player setup
-		p = UserInterface.PDB.register("Svend", 12, "FC København");
-		p2 = UserInterface.PDB.register("Jokum", 12, "FC Skive");
-		playerID = p.getID();
+		name = "Karl";
+		age = 14;
+		club ="Aab";
 		
 		// note setup
-		String speedText = "Hurtig";
-		int speedScore = 6;
-		String attitudeText = "God";
-		int attitudeScore = 4;
-		String techniqueText = "Middelmådig";
-		int techniqueScore = 3;
-		String gameSenseText = "Står godt på banen";
-		int gameSenseScore = 4;
-		n = p.addNote(s, speedText, speedScore, attitudeText, attitudeScore,
-				techniqueText, techniqueScore, gameSenseText, gameSenseScore);
+		speedText = "Hurtig";
+		speedScore = 6;
+		attitudeText = "God";
+		attitudeScore = 4;
+		techniqueText = "Middelmådig";
+		techniqueScore = 3;
+		gameSenseText = "Står godt på banen";
+		gameSenseScore = 4;
+		
+		//s = UserInterface.AGENDA.planSession(location, date);
+		//sessionID = s.getSessionID();
+		
+		// player setup
+		//p = UserInterface.PDB.register("Svend", 12, "FC København");
+		//p2 = UserInterface.PDB.register("Jokum", 12, "FC Skive");
+		//playerID = p.getID();
+		
+		// note setup
+		
+		//n = p.addNote(s, speedText, speedScore, attitudeText, attitudeScore,
+		//		techniqueText, techniqueScore, gameSenseText, gameSenseScore);
+	}
+	
+	@Before
+	public void setUp() throws Exception {
+		// setup a default scouting session to be used in some of the tests
+		UserInterface.AGENDA.planSession(location, date);
+	}
+	
+	@After
+	public void tearDown() throws Exception {
+		// clear the arraylist of players
+		UserInterface.PDB.getArrayListPlayer().clear();
+		
+		// clear the Agenda list of scouting sessions
+		// define a new arraylist
+		ArrayList<ScoutingSession> list = new ArrayList<ScoutingSession>();
+		// populate it with the same objects as the one in the agenda class
+		for(int i = 0; i < UserInterface.AGENDA.getNumberOfSessions(); i++){
+			ScoutingSession ss = UserInterface.AGENDA.getSessionByIndex(i);
+			list.add(ss);
+		}
+		// remove all the objects
+		for(ScoutingSession ss : list){
+			UserInterface.AGENDA.cancelSession(ss);
+		}
 	}
 	
 	@Test
 	public void testGetSession() {
-		ScoutingSession ss = n.getSession();
-		
-		assertEquals(ss, UserInterface.AGENDA.getSessionByID(sessionID));
-	}
-
-	@Test
-	public void testSetSession() {
-		Date date = UserInterface.DATEMANAGER.fromStringToDate("30-07-2015");
-		String location = "Odense";
+		// setup ScoutingSession
 		ScoutingSession ss = UserInterface.AGENDA.planSession(location, date);
-		
-		n.setSession(ss);
+		// setup player
+		Player p = UserInterface.PDB.register(name, age, club);
+		// setup note and add it to the player
+		Note n = p.addNote(ss, speedText, speedScore, attitudeText,
+				attitudeScore, techniqueText, techniqueScore, gameSenseText, gameSenseScore);
 		
 		assertEquals(ss, n.getSession());
 	}
 
 	@Test
-	public void testGetPlayer() {
-		Player player = n.getPlayer();
-		int pid = player.getID();
+	public void testSetSession() {
+		// setup ScoutingSession
+		ScoutingSession ss = UserInterface.AGENDA.planSession(location, date);
+		// setup player
+		Player p = UserInterface.PDB.register(name, age, club);
+		// setup note and add it to the player
+		Note n = p.addNote(ss, speedText, speedScore, attitudeText,
+				attitudeScore, techniqueText, techniqueScore, gameSenseText, gameSenseScore);
+
+		// create a new session and change it on the note
+		Date date = UserInterface.DATEMANAGER.fromStringToDate("30-07-2015");
+		String location = "Odense";
+		ScoutingSession ss2 = UserInterface.AGENDA.planSession(location, date);
+		n.setSession(ss2);
 		
-		assertEquals(player, UserInterface.PDB.getPlayerById(pid));
+		// check if the new one matches
+		assertEquals(ss2, n.getSession());
+	}
+
+	@Test
+	public void testGetPlayer() {
+		// setup ScoutingSession
+		ScoutingSession ss = UserInterface.AGENDA.planSession(location, date);
+		// setup player
+		Player p = UserInterface.PDB.register(name, age, club);
+		// setup note and add it to the player
+		Note n = p.addNote(ss, speedText, speedScore, attitudeText,
+				attitudeScore, techniqueText, techniqueScore, gameSenseText, gameSenseScore);
+		
+		// get the player from the note and assign it to a new variable 
+		Player p2 = n.getPlayer();
+		
+		// check if they're equal
+		assertEquals(p, p2);
 	}
 
 	@Test
 	public void testSetPlayer() {
+		// setup ScoutingSession
+		ScoutingSession ss = UserInterface.AGENDA.planSession(location, date);
+		// setup player
+		Player p = UserInterface.PDB.register(name, age, club);
+		// setup note and add it to the player
+		Note n = p.addNote(ss, speedText, speedScore, attitudeText,
+				attitudeScore, techniqueText, techniqueScore, gameSenseText, gameSenseScore);
+		
+		// setup a new player
+		String name = "Haakon";
+		int age = 19;
+		String club = "FC København";
+		Player p2 = UserInterface.PDB.register(name, age, club);
+		
+		// set the new player on the note
 		n.setPlayer(p2);
 		
+		// check if they're equal
 		assertEquals(p2, n.getPlayer());
 	}
 
 	@Test
 	public void testGetSpeedText() {
-		String speed = n.getSpeedText();
+		// setup ScoutingSession
+		ScoutingSession ss = UserInterface.AGENDA.planSession(location, date);
+		// setup player
+		Player p = UserInterface.PDB.register(name, age, club);
+		// setup note and add it to the player
+		Note n = p.addNote(ss, speedText, speedScore, attitudeText,
+				attitudeScore, techniqueText, techniqueScore, gameSenseText, gameSenseScore);
 		
-		assertEquals("Hurtig", speed);
+		// check if equals. We know from the setup that speed text is "Hurtig"
+		assertEquals("Hurtig", n.getSpeedText());
 	}
 
 	@Test
 	public void testSetSpeedText() {
+		// setup ScoutingSession
+		ScoutingSession ss = UserInterface.AGENDA.planSession(location, date);
+		// setup player
+		Player p = UserInterface.PDB.register(name, age, club);
+		// setup note and add it to the player
+		Note n = p.addNote(ss, speedText, speedScore, attitudeText,
+				attitudeScore, techniqueText, techniqueScore, gameSenseText, gameSenseScore);
+		
+		// setup new speed text and change it on the note
 		String speed = "Langsom";
 		n.setSpeedText(speed);
 		
@@ -90,15 +200,30 @@ public class NoteTest {
 
 	@Test
 	public void testGetSpeedScore() {
-		int speed = n.getSpeedScore();
+		// setup ScoutingSession
+		ScoutingSession ss = UserInterface.AGENDA.planSession(location, date);
+		// setup player
+		Player p = UserInterface.PDB.register(name, age, club);
+		// setup note and add it to the player
+		Note n = p.addNote(ss, speedText, speedScore, attitudeText,
+				attitudeScore, techniqueText, techniqueScore, gameSenseText, gameSenseScore);
 		
-		assertEquals(6, speed);
+		// check if equals. We know from the setup that the speed score is 6
+		assertEquals(6, n.getSpeedScore());
 	}
 
 	@Test
 	public void testSetSpeedScore() {
-		int speed = 2;
+		// setup ScoutingSession
+		ScoutingSession ss = UserInterface.AGENDA.planSession(location, date);
+		// setup player
+		Player p = UserInterface.PDB.register(name, age, club);
+		// setup note and add it to the player
+		Note n = p.addNote(ss, speedText, speedScore, attitudeText,
+				attitudeScore, techniqueText, techniqueScore, gameSenseText, gameSenseScore);
 		
+		// setup new speed score and change it on the note
+		int speed = 2;
 		n.setSpeedScore(speed);
 		
 		assertEquals(speed, n.getSpeedScore());
@@ -106,35 +231,61 @@ public class NoteTest {
 
 	@Test
 	public void testGetAttitudeText() {
-		String attitude = "Bakker godt op om holdkammerater";
+		// setup ScoutingSession
+		ScoutingSession ss = UserInterface.AGENDA.planSession(location, date);
+		// setup player
+		Player p = UserInterface.PDB.register(name, age, club);
+		// setup note and add it to the player
+		Note n = p.addNote(ss, speedText, speedScore, attitudeText,
+				attitudeScore, techniqueText, techniqueScore, gameSenseText, gameSenseScore);
 		
+		// check if equals. We know from the setup that attitude test is "God"
+		assertEquals("God", n.getAttitudeText());
+	}
+
+	@Test
+	public void testSetAttitudeText() {
+		// setup ScoutingSession
+		ScoutingSession ss = UserInterface.AGENDA.planSession(location, date);
+		// setup player
+		Player p = UserInterface.PDB.register(name, age, club);
+		// setup note and add it to the player
+		Note n = p.addNote(ss, speedText, speedScore, attitudeText,
+				attitudeScore, techniqueText, techniqueScore, gameSenseText, gameSenseScore);
+		
+		// setup a new attitude text and change it on the note
+		String attitude = "Dårlig";
 		n.setAttitudeText(attitude);
 		
 		assertEquals(attitude, n.getAttitudeText());
 	}
 
 	@Test
-	public void testSetAttitudeText() {
-		String attitude = "Dårlig";
-		
-		n.setAttitudeText(attitude);
-		
-		assertEquals("Dårlig", n.getAttitudeText());
-	}
-
-	@Test
 	public void testGetAttitudeScore() {
-		int attitude = 3;
+		// setup ScoutingSession
+		ScoutingSession ss = UserInterface.AGENDA.planSession(location, date);
+		// setup player
+		Player p = UserInterface.PDB.register(name, age, club);
+		// setup note and add it to the player
+		Note n = p.addNote(ss, speedText, speedScore, attitudeText,
+				attitudeScore, techniqueText, techniqueScore, gameSenseText, gameSenseScore);
 		
-		n.setAttitudeScore(attitude);
-		
-		assertEquals(attitude, n.getAttitudeScore());
+		// check if equals. We know from the setup that the attitude score is 4
+		assertEquals(4, n.getAttitudeScore());
 	}
 
 	@Test
 	public void testSetAttitudeScore() {
-		int attitude = 5;
+		// setup ScoutingSession
+		ScoutingSession ss = UserInterface.AGENDA.planSession(location, date);
+		// setup player
+		Player p = UserInterface.PDB.register(name, age, club);
+		// setup note and add it to the player
+		Note n = p.addNote(ss, speedText, speedScore, attitudeText,
+				attitudeScore, techniqueText, techniqueScore, gameSenseText, gameSenseScore);
 		
+		// setup new attitude score and change it on the note
+		int attitude = 5;
 		n.setAttitudeScore(attitude);
 		
 		assertEquals(attitude, n.getAttitudeScore());
@@ -142,17 +293,30 @@ public class NoteTest {
 
 	@Test
 	public void testGetTechniqueText() {
-		String technique = "Middelmådig";
+		// setup ScoutingSession
+		ScoutingSession ss = UserInterface.AGENDA.planSession(location, date);
+		// setup player
+		Player p = UserInterface.PDB.register(name, age, club);
+		// setup note and add it to the player
+		Note n = p.addNote(ss, speedText, speedScore, attitudeText,
+				attitudeScore, techniqueText, techniqueScore, gameSenseText, gameSenseScore);
 		
-		n.setTechniqueText(technique);
-		
-		assertEquals(technique, n.getTechniqueText());
+		// check if equal. We know from the setup that technique text should be "Middelmådig"		
+		assertEquals("Middelmådig", n.getTechniqueText());
 	}
 
 	@Test
 	public void testSetTechniqueText() {
-		String technique = "Forrygende";
+		// setup ScoutingSession
+		ScoutingSession ss = UserInterface.AGENDA.planSession(location, date);
+		// setup player
+		Player p = UserInterface.PDB.register(name, age, club);
+		// setup note and add it to the player
+		Note n = p.addNote(ss, speedText, speedScore, attitudeText,
+				attitudeScore, techniqueText, techniqueScore, gameSenseText, gameSenseScore);
 		
+		// setup new technique text and change it on the note
+		String technique = "Forrygende";
 		n.setTechniqueText(technique);
 		
 		assertEquals(technique, n.getTechniqueText());
@@ -160,17 +324,30 @@ public class NoteTest {
 
 	@Test
 	public void testGetTechniqueScore() {
-		int technique = 3;
+		// setup ScoutingSession
+		ScoutingSession ss = UserInterface.AGENDA.planSession(location, date);
+		// setup player
+		Player p = UserInterface.PDB.register(name, age, club);
+		// setup note and add it to the player
+		Note n = p.addNote(ss, speedText, speedScore, attitudeText,
+				attitudeScore, techniqueText, techniqueScore, gameSenseText, gameSenseScore);
 		
-		n.setTechniqueScore(technique);
-		
-		assertEquals(technique, n.getTechniqueScore());
+		// check if equal. We know from the setup that technique score should be 3
+		assertEquals(3, n.getTechniqueScore());
 	}
 
 	@Test
 	public void testSetTechniqueScore() {
-		int technique = 5;
+		// setup ScoutingSession
+		ScoutingSession ss = UserInterface.AGENDA.planSession(location, date);
+		// setup player
+		Player p = UserInterface.PDB.register(name, age, club);
+		// setup note and add it to the player
+		Note n = p.addNote(ss, speedText, speedScore, attitudeText,
+				attitudeScore, techniqueText, techniqueScore, gameSenseText, gameSenseScore);
 		
+		// setup new technique score and change it on the note
+		int technique = 5;		
 		n.setTechniqueScore(technique);
 		
 		assertEquals(technique, n.getTechniqueScore());
@@ -178,15 +355,30 @@ public class NoteTest {
 
 	@Test
 	public void testGetGameSenseText() {
-		String gameSense = n.getGameSenseText();
+		// setup ScoutingSession
+		ScoutingSession ss = UserInterface.AGENDA.planSession(location, date);
+		// setup player
+		Player p = UserInterface.PDB.register(name, age, club);
+		// setup note and add it to the player
+		Note n = p.addNote(ss, speedText, speedScore, attitudeText,
+				attitudeScore, techniqueText, techniqueScore, gameSenseText, gameSenseScore);
 		
+		// check if equal. We know from the setup that game sense text should be "Står godt på banen"
 		assertEquals("Står godt på banen", n.getGameSenseText());
 	}
 
 	@Test
 	public void testSetGameSenseText() {
-		String gameSense = "Står helt forkert på banen";
+		// setup ScoutingSession
+		ScoutingSession ss = UserInterface.AGENDA.planSession(location, date);
+		// setup player
+		Player p = UserInterface.PDB.register(name, age, club);
+		// setup note and add it to the player
+		Note n = p.addNote(ss, speedText, speedScore, attitudeText,
+				attitudeScore, techniqueText, techniqueScore, gameSenseText, gameSenseScore);
 		
+		// setup new game sense text and change it on the note
+		String gameSense = "Står helt forkert på banen";
 		n.setGameSenseText(gameSense);
 		
 		assertEquals(gameSense, n.getGameSenseText());
@@ -194,17 +386,30 @@ public class NoteTest {
 
 	@Test
 	public void testGetGameSenseScore() {
-		int gameSense = 5;
+		// setup ScoutingSession
+		ScoutingSession ss = UserInterface.AGENDA.planSession(location, date);
+		// setup player
+		Player p = UserInterface.PDB.register(name, age, club);
+		// setup note and add it to the player
+		Note n = p.addNote(ss, speedText, speedScore, attitudeText,
+				attitudeScore, techniqueText, techniqueScore, gameSenseText, gameSenseScore);
 		
-		n.setGameSenseScore(gameSense);
-		
-		assertEquals(gameSense, n.getGameSenseScore());
+		// check if equal. We know from the setup that game sense text should be 4
+		assertEquals(4, n.getGameSenseScore());
 	}
 
 	@Test
 	public void testSetGameSenseScore() {
-		int gameSense = 1;
+		// setup ScoutingSession
+		ScoutingSession ss = UserInterface.AGENDA.planSession(location, date);
+		// setup player
+		Player p = UserInterface.PDB.register(name, age, club);
+		// setup note and add it to the player
+		Note n = p.addNote(ss, speedText, speedScore, attitudeText,
+				attitudeScore, techniqueText, techniqueScore, gameSenseText, gameSenseScore);
 		
+		// setup new game sense score and change it on the note
+		int gameSense = 1;
 		n.setGameSenseScore(gameSense);
 		
 		assertEquals(gameSense, n.getGameSenseScore());
